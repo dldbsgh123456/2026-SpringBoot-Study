83b96331f63c9cddcffff66a5b1641dc6d00fd2a
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-08-03 11:42:48 +0900

    8/3

---
+++ SpringThymeLeafProject_1/src/main/java/com/sist/web/repository/BoardRepository.java
@@ -0,0 +1,42 @@
+package com.sist.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import com.sist.web.entity.*;
+// 단점 : JOIN이 어렵다 / SubQuery를 지원하지 않는다
+// MyBatis 8:2 JPA
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity,Integer>{
+    public BoardEntity findByNo(int no); // 상세보기
+    // 직접 SQL 문장 생성해도 됨 => findAll(Page)
+    @Query(value="SELECT * FROM board ORDER BY no DESC "
+                +"OFFSET :start ROWS FETCH NEXT 10 ROWS ONLY",
+                nativeQuery = true) // SQL을 JPQL로 변경없이 문장 그대로 
+    public List<BoardEntity> boardListData(@Param("start") Integer start);
+    /*
+     *   findBy
+     *   find SELECT *
+     *     By WHERE no=1 (int no)
+     *   
+     *   findByNameLike
+     *        WHERE name Like
+     *        
+     *   findByNoBetweenAnd(int a,int b)
+     *        WHERE name Like
+     *        
+     *   findByNameStartsWith A%
+     *        WHERE name Like
+     *   
+     *   findByNameEndsWith %A
+     *        WHERE name Like
+     *        
+     *   findByNameContainsh %A%
+     *       WHERE name Like
+     *   
+     */
+    // save(Update,insert) , delete
+}
